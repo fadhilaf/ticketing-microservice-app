595884e92dbd2f17ddc66e618c987d3be46038c2
FadhilAF <falfatih3421@gmail.com>
2023-07-11 06:16:35 +0700

    add loadbalancer service config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
