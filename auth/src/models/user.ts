1db1cfbba87b60d558ef8c5f6ffe79eea403617d
FadhilAF <falfatih3421@gmail.com>
2023-07-12 06:18:21 +0700

    add toJSON formatting for user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -15,16 +15,30 @@ interface UserModel extends Model<IUser> {
   build(user: IUser): Document<unknown, {}, IUser> & Omit<IUser & { _id: Types.ObjectId }, never>; //return type ny dapet dari "const user = new User()" type user ny
 }
 
-const userSchema = new Schema<IUser, UserModel>({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new Schema<IUser, UserModel>(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    //kalo mau customize response json dari model, kito bisa pake toJSON
+    toJSON: {
+      //ikuti cara serialize json dari mongoose
+      transform(doc, ret) {
+        delete ret.password; //kalo mau delete field dari object, kito bisa pake "delete {obj}.{field}". (syntax javascript yg jarang dipake)
+        delete ret.__v;
+        ret.id = ret._id; //kalo mau rename field dari object, kito bisa pake {new field name} = {old field name}.
+        delete ret._id;
+      },
+    },
+  }
+);
 userSchema.statics.build = (user: IUser) => {
   return new User(user);
 };
