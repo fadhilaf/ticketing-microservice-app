e66fbd8967aa652fc17e271093e9e8869df4ac7c
FadhilAF <falfatih3421@gmail.com>
2023-07-12 06:00:54 +0700

    add jwt, and kubernetes secret object type generic jwt-key

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { json } from "body-parser";
 import "express-async-errors"; // package ini harus diimport sebelum import routes, karena package ini akan ngehandle error yg terjadi di async function
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { signupRouter } from "./routes/signup";
 import { signinRouter } from "./routes/signin";
@@ -13,7 +14,14 @@ import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
+app.set("trust proxy", true); // express akan percaya bahwa proxy yg digunakan adalah proxy yg aman, sehingga express akan mengizinkan request dari proxy
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // disable cookie encryption (krn pake jwt)
+    secure: true, // cookie only works if user visit our app using https connection
+  })
+);
 
 app.use(signupRouter);
 app.use(signinRouter);
