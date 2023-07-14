4abe68fffcfb3a172bdc7eb283e6897c2760f939
FadhilAF <falfatih3421@gmail.com>
2023-07-15 06:15:09 +0700

    add initial api call for signup

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -1,21 +1,45 @@
-"use client"; //artiny jadikan ini component client side (kalo komponen server side, tidak perlu ditulis apa-apa [server component adalah component yang di render di server (tidak bisa menggunakan state, mengecek on event, cuma utk nge fetch data besar dalam satu page load, atau static page that needed lot of javascript)])
-import { useState } from "react";
+"use client"; //kalau pengen interaktif, harus pake client component. tapi kalo dk perlu interaktif trus cuma pengen fetch data pas restart bae, pake server component
+import { FormEvent } from "react";
+import axios from "axios";
+
+type ApiResponse = {
+  errors: Array<{
+    message: string;
+    field?: string;
+  }>;
+};
 
 export default function Signup() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  interface SignupFormElements extends HTMLFormControlsCollection {
+    email: HTMLInputElement;
+    password: HTMLInputElement;
+  }
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const elements = e.currentTarget.elements as SignupFormElements;
+
+    const response = await axios.post<ApiResponse>("/api/users/signup", {
+      email: elements.email.value,
+      password: elements.password.value,
+    });
+
+    response.data.errors.forEach((error) => {
+      console.log(error.message);
+    });
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <h1>Signup to indiestage</h1>
         <div className="border border-black">
-          <label>Email</label>
-          <input type="email" />
+          <label>Email: </label>
+          <input name="email" type="email" />
         </div>
         <div className="border border-black">
-          <label>Password</label>
-          <input type="password" />
+          <label>Password: </label>
+          <input name="password" type="password" />
         </div>
         <div className="border border-black">
           <button type="submit">Signup</button>
