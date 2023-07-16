a1fb1625baf510a6af8baa4b528756afa98d0adf
FadhilAF <falfatih3421@gmail.com>
2023-07-16 09:21:55 +0700

    finish sign up page

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body className={inter.className}>{children}</body>
     </html>
   );
--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -1,49 +1,120 @@
 "use client"; //kalau pengen interaktif, harus pake client component. tapi kalo dk perlu interaktif trus cuma pengen fetch data pas restart bae, pake server component
-import { FormEvent } from "react";
-import axios from "axios";
+import { useState, FormEvent } from "react";
+import axios, { AxiosResponse, AxiosError } from "axios";
 
-type ApiResponse = {
-  errors: Array<{
-    message: string;
-    field?: string;
-  }>;
+type ErrorApiResponse = {
+  errors: ErrorApiResponseField;
 };
 
+type ErrorApiResponseField = {
+  message: string;
+  field?: string;
+}[];
+
+interface SignupFormElements extends HTMLFormControlsCollection {
+  email: HTMLInputElement;
+  password: HTMLInputElement;
+}
+
 export default function Signup() {
-  interface SignupFormElements extends HTMLFormControlsCollection {
-    email: HTMLInputElement;
-    password: HTMLInputElement;
-  }
+  const [errors, setErrors] = useState<ErrorApiResponseField>([]);
+
+  const [successMessage, setSuccessMessage] = useState<string>("");
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
+
+  const [emailError, setEmailError] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<string>("");
+
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsButtonDisabled(true);
+    setErrors([]);
+    setEmailError("");
+    setPasswordError("");
 
     const elements = e.currentTarget.elements as SignupFormElements;
 
-    const response = await axios.post<ApiResponse>("/api/users/signup", {
-      email: elements.email.value,
-      password: elements.password.value,
-    });
+    try {
+      await axios.post<ErrorApiResponse>("/api/users/signup", {
+        email: elements.email.value,
+        password: elements.password.value,
+      });
 
-    response.data.errors.forEach((error) => {
-      console.log(error.message);
-    });
+      setSuccessMessage("Successfully signed up");
+    } catch (err) {
+      const error = err as AxiosError<ErrorApiResponse, any>;
+
+      error.response?.data.errors.forEach((error) => {
+        switch (error.field) {
+          case "email":
+            setEmailError(error.message);
+            break;
+          case "password":
+            setPasswordError(error.message);
+            break;
+          default:
+            setErrors((prev) => [...prev, error]);
+        }
+      });
+
+      setIsButtonDisabled(false);
+    }
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <h1>Signup to indiestage</h1>
-        <div className="border border-black">
-          <label>Email: </label>
-          <input name="email" type="email" />
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          {emailError ? (
+            <>
+              <input type="email" className="form-control is-invalid" name="email" id="email" />
+              <div id="email" className="invalid-feedback">
+                {emailError}
+              </div>
+            </>
+          ) : (
+            <input type="email" className="form-control" name="email" id="email" />
+          )}
         </div>
-        <div className="border border-black">
-          <label>Password: </label>
-          <input name="password" type="password" />
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          {passwordError ? (
+            <>
+              <input type="password" className="form-control is-invalid" id="password" />
+              <div id="password" className="invalid-feedback">
+                {passwordError}
+              </div>
+            </>
+          ) : (
+            <input type="password" className="form-control" id="password" />
+          )}
         </div>
-        <div className="border border-black">
-          <button type="submit">Signup</button>
-        </div>
+
+        <ul>
+          {errors.map((error, index) => (
+            <li>
+              <div key={index} className="alert alert-danger" role="alert">
+                {error.message}
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        {successMessage && (
+          <div className="alert alert-primary" role="alert">
+            {successMessage}
+          </div>
+        )}
+
+        <button type="submit" className="btn btn-primary" disabled={isButtonDisabled}>
+          Sign Up
+        </button>
       </form>
     </div>
   );
